7a22e9e6c8488e7d0eee0990ef5a4211cb09b9c4
Theo Ollivier <theo.ollivier@gmail.com>
2023-04-18 14:27:41 +0200

    Correction exo5

--- exercices/exo05/index.php
+++ exercices/exo05/index.php
@@ -4,22 +4,27 @@ $titre = "Exo 05 - Variables"; // Mettre le titre de la page
 
 <!-- mettre votre code ici -->
 <?php
-
-$nb1 = rand(1, 100);
-$nb2 = rand(1, 100);
-
-$resultat = abs($nb1 - $nb2);
-
-echo "Nombre 1 = <b>$nb1</b><br>";
-echo "Nombre 2 = <b>$nb2</b><br>";
-
-echo "Valeur absolue = $resultat<br>";
-
-if($resultat >= 25 && $resultat <= 75){
-    echo "La valeur absolue de ($nb1 - $nb2) est comprise entre 25 et 75";
+// création des deux nombres aléatoires
+$nb1 = rand(1,100);
+$nb2 = rand(1,100);
+// affichage de ces deux nombres
+echo "Nombre 1 : <b>{$nb1}</b><br>";
+echo "Nombre 2 : <b>{$nb2}</b><br>";
+// calcul de la différence entre le deuxième nombre et le premier
+$diff = $nb2 - $nb1;
+// calcul de la valeur absolue
+$valeurAbsolue = abs($diff);
+// affichage de la valeur absolue
+echo "Valeur absolue de <b>{$nb2}</b> - <b>{$nb1}</b> = <b>$valeurAbsolue</b><br>";
+// afficher si la valeur absolue est comprise entre 25 et 75
+echo "La valeur absolue de (<b>{$nb2}</b> - <b>{$nb1}</b>)";
+if ($valeurAbsolue >= 25 && $valeurAbsolue <= 75) {
+    echo " est ";
 } else {
-    echo "La valeur absolue de ($nb1 - $nb2) n'est pas comprise entre 25 et 75";
+    echo " n'est pas ";
 }
+
+echo "comprise entre 25 et 75";
 
 ?>
 
