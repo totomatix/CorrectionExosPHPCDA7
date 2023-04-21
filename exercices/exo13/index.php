570c938e671230adfe1902d39f93c6a6361decd2
Theo Ollivier <theo.ollivier@gmail.com>
2023-04-21 09:42:01 +0200

    Correction exo13

--- exercices/exo13/index.php
+++ exercices/exo13/index.php
@@ -4,6 +4,27 @@ $titre = "Exo 13 - Fonctions"; // Mettre le titre de la page
 <!-- mettre votre code ici -->
 <?php
 
+function factorielle(int $nb) : int
+{
+    $f = 1;
+    for ($i=2; $i <= $nb ; $i++) { 
+        $f *= $i;
+    }
+    return $f;
+}
+
+function factorielleRecursive(int $nb) : int
+{
+    if ($nb == 1) {
+        return 1;
+    }
+    return $nb * factorielleRecursive($nb-1);
+}
+
+
+$n1 = rand(1,15);
+echo "La factorielle de $n1 est " . factorielle($n1) . '<br>';
+echo "La factorielle recursive de $n1 est " . factorielleRecursive($n1);
 ?>
 
 <?php
