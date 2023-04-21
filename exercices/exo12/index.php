107ef446178fb8a17a96073f1f47cb680b30328a
Theo Ollivier <theo.ollivier@gmail.com>
2023-04-21 09:07:03 +0200

    Correction exo12

--- exercices/exo12/index.php
+++ exercices/exo12/index.php
@@ -4,7 +4,16 @@ $titre = "Exo 12 - Fonctions"; // Mettre le titre de la page
 
 <!-- mettre votre code ici -->
 <?php
+    function pair(int $nb) : bool
+    {
+        return ($nb%2==0);
+    }
 
+    $n1 = rand (1,100);
+    echo "Le nombre $n1 " .  ( (pair($n1)) ? "est pair." : "n'est pas pair.") . '<br>';
+
+    $n2 = rand (1,100);
+    echo "Le nombre $n2 " . ( (pair($n2)) ? "est pair." : "n'est pas pair.");
 ?>
 
 <?php
