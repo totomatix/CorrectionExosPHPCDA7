37fe2cd0aa6f1436235a461c0b3ade49ae57ec16
Theo Ollivier <theo.ollivier@gmail.com>
2023-05-02 08:44:24 +0200

    Correction exercice 14

--- exercices/exo14/index.php
+++ exercices/exo14/index.php
@@ -4,7 +4,23 @@ $titre = "Exo 14 - Fonctions"; // Mettre le titre de la page
 
 <!-- mettre votre code ici -->
 <?php
+function PGCD(int $nb1, int $nb2) : int
+{
+    $reste = -1;
+    $res = 0;
+    while ($reste != 0) {
+        $reste = $nb1 % $nb2;
+        if ($reste==0) {
+            return $res;
+        }
+        $res = $reste;
+        $nb1 = $nb2;
+        $nb2 = $reste;
+    }
+}
 
+echo "Le PGCD de 80 et 692 est : " . PGCD(80,692) . '<br>';
+echo "Le PGCD de 56952 et 3444 est : " . PGCD(56952,3444);
 ?>
 
 <?php
