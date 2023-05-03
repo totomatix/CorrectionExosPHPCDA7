4ad2b8d4cb227a966e911312781e0a16d585d9be
Theo Ollivier <theo.ollivier@gmail.com>
2023-05-03 09:13:46 +0200

    Correction exo16

--- exercices/exo16/index.php
+++ exercices/exo16/index.php
@@ -3,7 +3,19 @@ $titre = "Exo 16 - Tableaux"; // Mettre le titre de la page
 ?>
 <!-- mettre votre code ici -->
 <?php
+$noms_masculins = ['Marc','Luc','Clément','Vincent'];
+$noms_feminins = array('Mathilde','Lucie','Véronique');
 
+// affichage 1er tableau
+for ($i=0; $i < count($noms_masculins); $i++) { 
+    echo "$i : $noms_masculins[$i]<br>";
+}
+
+echo '******************<br>';
+// affichage 2eme tableau
+foreach ($noms_feminins as $indice => $prenom) {
+    echo "$indice : $prenom<br>";
+}
 ?>
 
 <?php
