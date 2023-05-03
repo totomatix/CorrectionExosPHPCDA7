5a057892c5f236dae6a338cf98de1a6e7fa3f7d1
Theo Ollivier <theo.ollivier@gmail.com>
2023-05-03 09:03:51 +0200

    Correction exo 15

--- exercices/exo15/index.php
+++ exercices/exo15/index.php
@@ -4,7 +4,14 @@ $titre = "Exo 15 - Tableaux"; // Mettre le titre de la page
 
 <!-- mettre votre code ici -->
 <?php
+function supprimer_voyelles(string $mot) : string
+{
+    $voyelles = ['a','e','i','o','u','y','A','E','I','O','U','Y'];
+    return str_replace($voyelles,"",$mot);
+}
 
+$mot = "bordure";
+echo "Le mot <b>\"$mot\"</b> sans voyelles est <b>\"" . supprimer_voyelles($mot) . "\"</b>";
 ?>
 
 <?php
