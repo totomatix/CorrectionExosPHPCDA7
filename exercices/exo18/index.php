dac136516d1828996d3fcb79be7a3c910bfbf44d
Theo Ollivier <theo.ollivier@gmail.com>
2023-05-04 11:07:25 +0200

    Correction exos 18 et 19

--- exercices/exo18/index.php
+++ exercices/exo18/index.php
@@ -4,6 +4,24 @@ $titre = "Exo 18 - Les tableaux"; // Mettre le titre de la page
 <!-- mettre votre code ici -->
 <?php
 
+// création du tableau de tableaux de notes
+$tableau_notes = [
+    [12,4,7,18],
+    [14,14,17,8],
+    [2,4,7,9],
+    [12,4,7,18,20]
+];
+
+// affichage de la moyenne des notes de chaque élève
+foreach ($tableau_notes as $index => $notes_eleve) {
+    echo "La moyennne des notes de l'élève <b>" . ($index+1) . '</b> est de <b> ' . moyenne($notes_eleve) . '</b><br>'; 
+}
+
+// Mes fonctions
+function moyenne(array $tab) : float
+{
+    return round(array_sum($tab)/count($tab),2);
+}
 ?>
 
 <?php
--- exercices/exo19/index.php
+++ exercices/exo19/index.php
@@ -3,7 +3,27 @@ $titre = "Exo 19 - Tableaux"; // Mettre le titre de la page
 ?>
 <!-- mettre votre code ici -->
 <?php
+$personnes = [
+    'personne1' => ['nom' => 'Clément', 'age' => 25, 'sexe' => false],
+    'personne2' => ['nom' => 'Mathilde', 'age' => 19, 'sexe' => true]
+];
+AfficherPersonnes($personnes);
 
+function AfficherPersonnes($tab_personnes)
+{
+    // parcours du 1er niveau du tableau
+    foreach ($tab_personnes as $personne) {
+        // parcours du tableau de chaque personne
+        foreach ($personne as $propriete => $value) {
+            if ($propriete != 'sexe') {
+                echo "$propriete : $value<br>";
+            } else {
+                echo "$propriete : " . (($value) ? 'femme' : 'homme') . '<br>';
+            }
+        }
+        echo "=============<br>";
+    }
+}
 ?>
 
 <?php
