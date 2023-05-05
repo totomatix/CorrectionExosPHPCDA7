8f7681a906501102104fb98359163588b239cc30
Theo Ollivier <theo.ollivier@gmail.com>
2023-05-05 12:18:42 +0200

    Correction Exos 21 et 22

--- exercices/exo21/index.php
+++ exercices/exo21/index.php
@@ -2,15 +2,33 @@
 $titre = "Exo 21 - Formulaires"; // Mettre le titre de la page
 ?>
 <!-- mettre votre code ici -->
-<?php
+<form action="" method="get">
+    <label for="pseudo"></label>
+    <input type="text" name="pseudo" id="pseudo" required>
+    <label for="age"></label>
+    <input type="number" name="age" id="age" required>
+    <button type="submit">Valider</button>
+</form>
 
+<?php
+    $pseudo = '';
+    if (isset($_GET['pseudo'])) {
+        $pseudo = htmlentities($_GET['pseudo']);
+    }
+    $age = '';
+    if (isset($_GET['age'])) {
+        $age = htmlentities($_GET['age']);
+    }
+    if (!empty($pseudo) && !empty($age)) {
+        echo "$pseudo a $age ans.";
+    }
 ?>
 
 <?php
-    /**
-     * Ne pa modifier
-     * permet d'inclure le menu et le template
-     */
-    $content = ob_get_clean(); 
-    require "../../partials/layout.php";
+/**
+ * Ne pa modifier
+ * permet d'inclure le menu et le template
+ */
+$content = ob_get_clean();
+require "../../partials/layout.php";
 ?>
--- exercices/exo22/index.php
+++ exercices/exo22/index.php
@@ -1,9 +1,23 @@
 <?php ob_start(); //  ne pas modifier
 $titre = "Exo 22 - Formulaires"; // Mettre le titre de la page
 ?>
-<!-- mettre votre code ici -->
-<?php
 
+<form action="" method="post">
+    <label for="nombre"></label>
+    <input type="number" name="nombre" id="nombre" required>
+    <button type="submit">Valider</button>
+</form>
+
+<?php
+    // $nombre = '';
+    if (isset($_POST['nombre'])) {
+        $nombre = htmlentities($_POST['nombre']);
+    }
+    echo $nombre.'<br>';
+    var_dump(empty($nombre));
+    if (!empty($nombre) || $nombre==='0') {
+        echo "Le nombre $nombre est ". (($nombre%2==0) ? "pair." : "impair.");
+    }
 ?>
 
 <?php
